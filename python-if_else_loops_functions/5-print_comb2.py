af37e0134aabdf6b1276058527ccfcc81ed2641b
kjuarez38 <kaisjuarez@gmail.com>
2024-01-22 14:30:27 +0100

    print comb2

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    if i == 99:
+        print("{:d}".format(i))
+    elif i < 10:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:d}".format(i), end=", ")
