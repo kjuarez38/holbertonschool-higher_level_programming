6d712835b0f0a281d687b11a0c207d6f7f308bb0
kjuarez38 <kaisjuarez@gmail.com>
2024-01-22 15:49:31 +0100

    fizz buzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if (i % 3 == 0):
+            print("Fizz", end="")
+        elif (i % 5 == 0):
+            print("Buzz", end="")
+
+        if (i % 3 != 0 and i % 5 != 0):
+            print("{:d}".format(i), end="")
+
+        if (i != 100):
+            print(" ", end="")
