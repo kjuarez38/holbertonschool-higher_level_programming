6c93ccff2469266bf040984fe08f38132bbf703a
kjuarez38 <kaisjuarez@gmail.com>
2024-02-11 20:22:38 +0100

    add integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,11 +4,15 @@
 
 def add_integer(a, b=98):
     """Add integer function"""
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    try:
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        if not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
+        return int(a) + int(b)
+
+    except Exception as e:
+        return e
 
 if __name__ == "__main__":
     import doctest
