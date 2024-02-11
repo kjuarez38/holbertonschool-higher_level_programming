24c2a08bbec23af088c13b5fa96585ec49988f7a
kjuarez38 <kaisjuarez@gmail.com>
2024-02-12 00:41:45 +0100

    rectangle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Rectangle class"""
+"""Rectangle"""
 
 
 class Rectangle:
-    """Rectangle class"""
+    """Rectangle"""
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
-        self.number_of_instances = Rectangle.number_of_instances
 
     @property
     def width(self):
@@ -32,7 +31,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -50,8 +49,9 @@ class Rectangle:
         string = ""
         if self.width == 0 or self.height == 0:
             return string
-        return "\n".join(str(self.print_symbol) *
-                         self.width for _ in range(self.height))
+        for h in range(self.__height):
+            string += str(str(self.print_symbol)*self.__width) + "\n"
+        return str(string[:-1])
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.width, self.height)
@@ -60,15 +60,16 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
+        if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
+        if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        else:
-            return rect_2
-    
+        return rect_2
+
+    @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return Rectangle(cls, cls)
