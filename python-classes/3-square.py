615096cc192abdffe6d740f2c106fcce79bcb04b
kjuarez38 <kaisjuarez@gmail.com>
2024-02-11 19:47:51 +0100

    square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,9 +6,10 @@ class Square:
     """init class Square"""
     def __init__(self, size=0):
         self.__size = size
-        if type(size) != int:
+        if not isinstance(size, int):
             raise Exception("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         return self.__size * self.__size
