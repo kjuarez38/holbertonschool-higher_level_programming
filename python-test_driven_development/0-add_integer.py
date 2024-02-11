dddc42b136ff090613edf2a1f2586985263123e1
kjuarez38 <kaisjuarez@gmail.com>
2024-02-11 20:17:19 +0100

    add integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Add integer module"""
+
+
+def add_integer(a, b=98):
+    """Add integer function"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
