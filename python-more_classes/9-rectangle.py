0739516222747bc1aa5ab99f1cd7a57aa151e2d8
kjuarez38 <kaisjuarez@gmail.com>
2024-02-12 00:42:25 +0100

    rectangle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -60,7 +60,6 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
@@ -70,6 +69,5 @@ class Rectangle:
             return rect_1
         return rect_2
 
-    @classmethod
     def square(cls, size=0):
         return Rectangle(cls, cls)
