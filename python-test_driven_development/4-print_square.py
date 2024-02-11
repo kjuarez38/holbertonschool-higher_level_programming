24ebb8912b364b41338c59e9622620247b13616a
kjuarez38 <kaisjuarez@gmail.com>
2024-02-11 21:50:33 +0100

    print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,10 +3,13 @@
 
 
 def print_square(size):
-    """Print square function"""
+    """Function that prints a square with the character #."""
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(size):
-        print("#" * size)
+
+    for y in range(0, size):
+        for x in range(0, size):
+            print("#", end="")
+        print()
