48889f202b557d4a497505b89dfd1bc132747eb5
kjuarez38 <kaisjuarez@gmail.com>
2024-02-12 02:08:40 +0100

    inherits from

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""inherits_from module"""
+
+
+def inherits_from(obj, a_class):
+    """inherits_from function"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
