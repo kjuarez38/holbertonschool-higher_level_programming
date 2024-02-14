f3234aa2a0c491a43cf3ca81e688ca79ffb5b390
kjuarez38 <kaisjuarez@gmail.com>
2024-02-14 19:07:47 +0100

    pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""pascal triangle"""
+
+
+def pascal_triangle(n):
+    """pascal triangle"""
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [None for _ in range(i + 1)]
+        row[0], row[-1] = 1, 1
+
+    for j in range(1, len(row) - 1):
+        row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+    
+    triangle.append(row)
+
+    return triangle
