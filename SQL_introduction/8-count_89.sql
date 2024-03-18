50f47d6f21299c95980122b8d8d3f93bc6b4ece5
kjuarez38 <kaisjuarez@gmail.com>
2024-03-18 20:00:21 +0100

    display number pf record

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,4 @@
 -- 8. Count 89 in first_table.
-SELECT COUNT(*) AS record_count FROM hbtn_0c_0.first_table WHERE id = 89;
+SELECT COUNT(*) AS record_count 
+FROM hbtn_0c_0.first_table 
+WHERE id = 89;
