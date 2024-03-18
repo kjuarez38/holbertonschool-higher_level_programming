73298e70bb03d7943bc2260520200ef1a324e6df
kjuarez38 <kaisjuarez@gmail.com>
2024-03-18 20:06:37 +0100

    find the best score

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- 11. Find the best score of each student.
-SELECT score, name
-FROM hbtn_0c_0.second_table
-WHERE score >= 10
-ORDER BY score DESC;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
