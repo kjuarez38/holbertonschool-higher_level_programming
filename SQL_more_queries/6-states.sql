b3e1f5fe7ed8911431f84d5eec9c715e8cdc784e
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 12:20:04 +0100

    state ok

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- 6.states.sql creates the database hbtn_0d_usa and the table states
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL,
+);
