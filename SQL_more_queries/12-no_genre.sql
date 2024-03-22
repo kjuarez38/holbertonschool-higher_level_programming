cd00ecc1f249674b8537de7f8dd17e88d403d8f0
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 13:18:29 +0100

    list without genre linked

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- 12.no_genre.sql starts here
-Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+-- write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
         LEFT JOIN `tv_show_genres` AS g
