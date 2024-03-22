0074b340208cd5a9cdee409305d9c3ee2547a49c
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 13:09:51 +0100

    genre id all shows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
--- 10.genre_id_by_show.sql starts here
--- that lists all shows contained in the database hbtn_0d_tvshows
+-- 11.genre_id_all_shows.sql creates a SQL script that lists all shows contained in the database hbtn_0d_tvshows
+-- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
         LEFT JOIN `tv_show_genres` AS g
