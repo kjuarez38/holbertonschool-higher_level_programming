c2da62383e65bb458a6539d84862da16dcc2e628
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 13:59:01 +0100

    list all comedy show

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,11 +1,10 @@
 -- 15.comedy_only.sql starts here
 -- write a script that lists all Comedy shows in the database hbtn_0d_tvshows
-SELECT g.`title`
-  FROM `tv_shows` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`show_id`
+SELECT s.`title`, t.`name`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON g.`id` = g.`show_id`
 
-       INNER JOIN `tv_genres` AS t
-       ON t.`id` = s.`genre_id`
-       WHERE t.`name` = "Comedy"
- ORDER BY g.`title`;
+       LEFT JOIN `tv_genres` AS t
+       ON t.`id` = g.`genre_id`
+ ORDER BY s.`title`;
