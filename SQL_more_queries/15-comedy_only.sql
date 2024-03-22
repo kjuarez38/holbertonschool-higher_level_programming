1d6dc051266570203758c388b6a479efe86c00e6
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 13:54:40 +0100

    comedy only

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -3,7 +3,7 @@
 SELECT g.`title`
   FROM `tv_shows` AS g
        INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`genre_id`
+       ON g.`id` = s.`show_id`
 
        INNER JOIN `tv_genres` AS t
        ON t.`id` = s.`genre_id`
