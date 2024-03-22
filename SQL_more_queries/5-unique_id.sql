fc320d687798e523c0a2c3de750e61e932929ebe
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 12:16:37 +0100

    unique id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
--- 5. Unique ID create the table unique_id
+-- 5. Unique ID create
 CREATE TABLE IF NOT EXISTS `unique_id` (
-    `id` INT(1) UNIQUE
+    `id` INT DEFAULT 1 UNIQUE,
     `name` VARCHAR(256) 
 );
