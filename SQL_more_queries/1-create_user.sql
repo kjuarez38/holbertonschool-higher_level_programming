a99486b4699cb88703b14bf7d0aa4d054841e2ef
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 11:51:59 +0100

    create user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- 1.create_user.sql creates the users user_0d_1 and user_0d_2
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
