a8b77aae79eb353cae9c57d4fe59864fb358a83e
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 12:26:26 +0100

    cities ok

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- 7.cities.sql creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `state_id` INT(11) NOT NULL FOREIGN KEY REFERENCES states(`id`),
+    `name` VARCHAR(256) NOT NULL, 
+    PRIMARY KEY (`id`)
+);
