f3b6071cf0e72f948442e1292170436085788c16
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 11:56:46 +0100

    create database and user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- 2.create_read_user.sql creates the database hbtn_0d_2 and user_0d_2
+CREATE DATABASE 
+    IF NOT EXISTS hbtn_0d_2;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
