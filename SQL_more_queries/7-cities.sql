59333a179efc62c6f13e8090510499d05aff5ba0
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 12:30:59 +0100

    cities ok

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,7 +3,9 @@ CREATE DATABASE
     IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
     `id` INT(11) DEFAULT UNIQUE NOT NULL AUTO_INCREMENT,
-    `state_id` INT(11) NOT NULL FOREIGN KEY REFERENCES states(`id`),
+    `state_id` INT(11) NOT NULL,
     `name` VARCHAR(256) NOT NULL, 
     PRIMARY KEY (`id`)
+    FOREIGN KEY (`state_id`) 
+    REFERENCES states(`id`)
 );
