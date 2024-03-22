a1e73fa6728cc5835de8f48fab036ea1c7a2cc9e
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 11:59:48 +0100

    create database and user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,12 @@
 -- 2.create_read_user.sql creates the database hbtn_0d_2 and user_0d_2
-CREATE DATABASE 
-    IF NOT EXISTS hbtn_0d_2;
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES
-    ON hbtn_0d_2.*
-    TO 'user_0d_2'@'localhost';
-    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
