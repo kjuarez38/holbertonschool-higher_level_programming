aeb9ed1c61ed83ae66e1892aa6501b768819c457
kjuarez38 <kaisjuarez@gmail.com>
2024-03-22 13:01:25 +0100

    genre tv show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,5 +4,5 @@ SELECT `tv_shows`.`title`, `tv_shows_genres`.`genres_id`,
 FROM `tv_shows`
 JOIN `tv_shows_genres`
 ON `tv_shows`.`id` = `tv_shows_genres`.`tv_show_id`;
-ORDER BY tv_shows.title ASC, 
-tv_show_genres.genre_id ASC;
+ORDERED BY `tv_shows`.`title` ASC;
+```
